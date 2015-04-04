a9413d9e385cfbc90e7f2191d2e8f46fe3988e04
Marc Abi-Samra <mas1529@nyu.edu>
2015-04-04 14:43:54 -0400

    basic functionality in place

---
+++ server.js
@@ -0,0 +1,39 @@
+var io = require('socket.io'),
+	connect = require('connect'),
+	app = connect().use(connect.static('public')).listen(8000),
+	room = io.listen(app);
+
+var ID = 0,
+	emotionListLength;
+
+
+
+
+room.sockets.on('connection', function(socket){
+
+	socket.emit('entrance', {
+		message:'Welome to Twitch Plays Hamlet. Choose an emotion for our actors.',
+		id: ID,
+	});
+	ID++;
+
+	socket.on('emotions', function(data){
+		emotionListLength = data.l;
+		console.log("There are this many emotions: " + emotionListLength);
+
+		(function(){
+			socket.emit('newEmotion', {emotionOne: Math.floor(Math.random() * emotionListLength),
+			emotionTwo: Math.floor(Math.random() * emotionListLength)})
+		})();
+
+		setInterval(function(){
+			socket.emit('newEmotion', {emotionOne: Math.floor(Math.random() * emotionListLength),
+			emotionTwo: Math.floor(Math.random() * emotionListLength)})
+		}, 3000)
+	})
+
+	socket.on('vote', function(data){
+		console.log(data.vote);
+	})
+
+})
